b5e1023ce279ca990d216e8ae372f9cd89636a74
Nikhil <nikhilbhatia2004@gmail.com>
2018-09-28 17:14:20 +0300

    newewewe

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -18,6 +18,7 @@ class SearchBar extends React.Component {
     this.handleTermChange=this.handleTermChange.bind(this);
     this.handleLocationChange=this.handleLocationChange.bind(this);
     this.handleSearch=this.handleSearch.bind(this);
+    this.handleKeypress=this.handleKeypress.bind(this)
   }
     renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
@@ -39,18 +40,26 @@ class SearchBar extends React.Component {
   }
 
   handleTermChange(event) {
-    this.setState({term:event.target.value})
+    this.setState({term:event.target.value});
   }
   handleLocationChange(event) {
     this.setState({location:event.target.value})
   }
-  handleSearch(event) {
-    this.props.searchYelp(this.state.term,this.state.location,this.state.sortBy);
-    event.preventDefault()
+  handleKeypress(event) {
+    if (event.key==='Enter') {
+      this.handleSearch()
+    }
+  }
+  handleSearch() {
+    if (this.state.term && this.state.location)
+    {this.props.searchYelp(this.state.term,this.state.location,this.state.sortBy);
+    console.log("Search")}
   }
   handleSortByChange(sortByOption) {
     this.setState({sortBy:sortByOption});
-      }
+    setTimeout(this.handleSearch, 0);
+    this.handleSearch()
+          }
 
   render() {
     return (<div className="SearchBar">
@@ -59,14 +68,13 @@ class SearchBar extends React.Component {
   {this.renderSortByOptions()}
     </ul>
   </div>
-  <form>
   <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" onChange={this.handleTermChange} />
-    <input placeholder="Where?" onChange={this.handleLocationChange}/>
+    <input placeholder="Search Businesses" onChange={this.handleTermChange} onKeyPress={this.handleKeypress}/>
+    <input placeholder="Where?" onChange={this.handleLocationChange} onKeyPress={this.handleKeypress}/>
   </div>
 <div className="SearchBar-submit">
     <button className="button" onClick={this.handleSearch}>Let's Go</button>
-  </div></form>
+  </div>
 </div>);
   }
 }
